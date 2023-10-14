2c8356b57b434816b8e7b1c5d6a7ce7d39aa4adb
MG2003 <max.guo2014@gmail.com>
2023-10-14 13:16:25 -0400

    Moved files

--- scripts/droppable.gd
+++ scripts/droppable.gd
@@ -3,5 +3,5 @@ extends Resource
 class_name Droppable_Block
 
 @export var block_shape: PackedScene
-@export var block_thumb: Image
+@export var block_thumb: CompressedTexture2D
 @export var name: String

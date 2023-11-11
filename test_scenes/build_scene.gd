00da9c8ef416b2f612b8d1d12bea6edad8676008
evan-mick <evsammick@gmail.com>
2023-11-11 16:16:14 -0500

    Bad scaling, fixes

--- test_scenes/build_scene.gd
+++ test_scenes/build_scene.gd
@@ -20,8 +20,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(Input.is_action_just_released("queuePop")):
-		drop_block()
+	pass
+#	if(Input.is_action_just_released("queuePop")):
+#		drop_block()
 	
 
 func drop_block():

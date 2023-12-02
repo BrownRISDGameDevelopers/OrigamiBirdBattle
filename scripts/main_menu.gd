e7c92215a4fdb1194f43b71e77310cd86b595f53
MG2003 <max.guo2014@gmail.com>
2023-12-02 15:15:27 -0500

    bg-blur on tutorial and main menu implementation

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -7,7 +7,6 @@ extends CanvasLayer
 @export var credits_to: String
 @export var tutorial_to: String
 @onready var start_game_button = $Start
-@onready var tutorial_button = $Tutorial
 @onready var credits_button = $Credits
 @onready var quit_button = $Quit
 
@@ -16,16 +15,10 @@ func _ready():
 	start_game_button.button_down.connect(on_start_button_down)
 	credits_button.button_down.connect(on_credit_button_down)
 	quit_button.button_down.connect(on_quit_button_down)
-	tutorial_button.button_down.connect(on_tutorial_button_down)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
-
-func on_tutorial_button_down():
-#	scene_to = "prototype"
-	get_tree().change_scene_to_file("res://levels/" + tutorial_to + ".tscn")
-#	get_tree().change_scene_to_packed(tutorial_to)
 
 func on_start_button_down():
 #	scene_to = "prototype"

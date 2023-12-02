5dd050b587d6f6892af27c56f8ddb1d957bafd25
MG2003 <max.guo2014@gmail.com>
2023-12-02 18:23:40 -0500

    fully polished tutorial

--- tutorial.gd
+++ tutorial.gd
@@ -16,19 +16,39 @@ func _ready():
 
 
 func _on_next_pressed():
-	if (cur_text_in < textures.size() - 1):
+	$button_pressed.play()
+	await get_tree().create_timer(0.40).timeout
+	$button_pressed.stop()
+	if (cur_text_in < textures.size() - 2):
 		cur_text_in += 1
 		text_rect.texture = textures[cur_text_in]
 		$back.show()
 	else:
-		get_tree().change_scene_to_file("res://levels/" + goto_scene_on_end + ".tscn")
+		cur_text_in += 1
+		text_rect.texture = textures[cur_text_in]
+		$next.hide()
+		$start_button.show()
+	
 	pass # Replace with function body.
 
 
 func _on_back_pressed():
+	$button_pressed.play()
+	await get_tree().create_timer(0.40).timeout
+	$button_pressed.stop()
 	if (cur_text_in > 0):
 		cur_text_in -= 1
 		text_rect.texture = textures[cur_text_in]
+		$next.show()
+		$start_button.hide()
 	if (cur_text_in == 0):
 		$back.hide()
+	pass # Replace with function body.
+
+
+func _on_start_button_pressed():
+	$start.play()
+	await get_tree().create_timer(0.40).timeout
+	$start.stop()
+	get_tree().change_scene_to_file("res://levels/" + goto_scene_on_end + ".tscn")
 	pass # Replace with function body.

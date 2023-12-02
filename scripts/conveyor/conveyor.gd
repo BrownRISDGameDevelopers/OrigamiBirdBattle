8b925c732820a0e94137da04127f95808674372b
Dustin Wu <dustin.h.wu@gmail.com>
2023-12-02 14:51:38 -0500

    fixed issue where blocks could be dropped instantly after they were stacked high enough

--- scripts/conveyor/conveyor.gd
+++ scripts/conveyor/conveyor.gd
@@ -3,12 +3,15 @@ class_name Conveyor
 
 @export var item_num: int = 20
 @onready var conveyor_display: Array = $NinePatchRect/conveyor_grid.get_children()
+@onready var block_timer: Timer = $block_timer
 signal drop_block(blockdata)
 
 const FILEPATH = "res://resources/block_resources/"
 var queue = []
 var blocks = DirAccess.open(FILEPATH).get_files()
 var block_free = true
+var block_contacted_ground = false
+var block_timer_timeout = false
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -49,9 +52,20 @@ func pop_block():
 func try_pop():
 	if block_free:
 		block_free = false
+		block_contacted_ground = false
+		block_timer_timeout = false
+		block_timer.start()
 		pop_block()
 	
 		
 func _setActive():
-	block_free = true
-	print("active again")
+	#print("block contacted ground")
+	block_contacted_ground = true
+	if block_timer_timeout:
+		block_free = true
+
+func _on_block_timer_timeout():
+	#print("block timer timeout")
+	block_timer_timeout = true
+	if block_contacted_ground:
+		block_free = true
